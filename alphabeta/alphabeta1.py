4c691b2ee83eb325685e134a34b6b24f987773a5
IQBAL BIN HAFIZ <hafiz2305341073@diu.edu.bd>
2026-08-29 23:02:41 +0600

    alpha1

---
+++ alphabeta/alphabeta1.py
@@ -0,0 +1,26 @@
+def alphabeta(depth, node , ismax,values , alpha , beta):
+    if depth == 3:
+        return values[node]
+    if ismax:
+        best = float("-inf")
+        for i in range(2):
+            value = alphabeta(depth+1,node*2+i,False,values,alpha,beta)
+            best = max(best,value)
+            alpha = max(alpha,best)
+            if beta<=alpha:
+                break
+        return best
+    else:
+        best = float("inf")
+        for i in range(2):
+            value = alphabeta(depth+1,node*2+i,True,values,alpha,beta)
+            best = min(best,value)
+            beta = min(beta,best)
+            if beta<=alpha:
+                break
+        return best
+
+
+values = [3, 5, 6, 9, 1, 2, 0, -1]
+result = alphabeta(0,0,True,values,float("-inf"),float("inf"))
+print("Best value is = ",result)
